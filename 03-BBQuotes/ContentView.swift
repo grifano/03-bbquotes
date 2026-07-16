0d9b7dc30408925f459cdd39d00a19f33be7e83d
Serhii Orlenko <sorlenk@gmail.com>
2026-07-16 11:34:51 +0200

    Initial Commit

---
+++ 03-BBQuotes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  03-BBQuotes
+//
+//  Created by sorlenko on 16/07/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 03-BBQuotes/_3_BBQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  _3_BBQuotesApp.swift
+//  03-BBQuotes
+//
+//  Created by sorlenko on 16/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct _3_BBQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
