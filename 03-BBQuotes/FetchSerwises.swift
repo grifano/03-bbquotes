6b3b18765bb267c13bdaeb8c8aead722f250e2df
Serhii Orlenko <sorlenk@gmail.com>
2026-07-20 20:12:07 +0200

    Add more fetch functions

--- 03-BBQuotes/FetchSerwises.swift
+++ 03-BBQuotes/FetchSerwises.swift
@@ -30,4 +30,43 @@ struct FetchSerwises {
         
         return quote
     }
+    
+    func fetchCharacter(_ name: String) async throws -> CharacterModel {
+        let characterUrl = baseUrl.appendingPathExtension("characters")
+        let fetchUrl = characterUrl.appending(queryItems: [URLQueryItem(name: "name", value: name)])
+        
+        let (data, response) = try await URLSession.shared.data(from: fetchUrl)
+        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+            throw FetchError.badResponse
+        }
+        
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        
+        let characters = try decoder.decode([CharacterModel].self, from: data)
+        
+        return characters[0]
+    }
+    
+    func fetchDeath(for character: String) async throws -> DeathModel? {
+        let fetchUrl = baseUrl.appending(path: "death")
+        
+        let (data, response) = try await URLSession.shared.data(from: fetchUrl)
+        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+            throw FetchError.badResponse
+        }
+        
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        
+        let deaths = try decoder.decode([DeathModel].self, from: data)
+        
+        for death in deaths {
+            if death.character == character {
+                return death
+            }
+        }
+        
+        return nil
+    }
 }
