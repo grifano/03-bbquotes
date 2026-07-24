01558bdb5d6b0288d075251596487fc882b01d58
Serhii Orlenko <sorlenk@gmail.com>
2026-07-24 14:06:34 +0200

    Add breakingbad color set, add spacer, test quote length to fit the screen

--- 03-BBQuotes/QuoteView.swift
+++ 03-BBQuotes/QuoteView.swift
@@ -21,7 +21,9 @@ struct QuoteView: View {
                     .frame(width: geo.size.width * 2.7, height: geo.size.height * 1.2)
                 
                 VStack {
+                    Spacer(minLength: 100)
                     Text("\"\(vm.quote.quote)\"")
+                        .minimumScaleFactor(0.5)
                         .font(.system(size: 20, design: .serif))
                         .foregroundStyle(.white)
                         .multilineTextAlignment(.center)
@@ -30,17 +32,17 @@ struct QuoteView: View {
                         .clipShape(.rect(cornerRadius: 20))
                         .padding(.horizontal, 20)
                         .frame(maxWidth: geo.size.width)
-                        .padding(.bottom, 12)
+                        .padding(.bottom, 8)
                     
                     ZStack(alignment: .bottom) {
-                        AsyncImage(url: vm.character.images[0]) { image in
+                        AsyncImage(url: vm.character.images[5]) { image in
                             image
                                 .resizable()
                                 .scaledToFill()
                         } placeholder: {
                             ProgressView()
                         }
-                        .frame(width: geo.size.width / 1.4, height: geo.size.height / 2)
+                        .frame(width: geo.size.width / 1.4, height: geo.size.height / 2.2)
                         
                         Text(vm.character.name)
                             .font(.system(size: 18))
@@ -49,7 +51,7 @@ struct QuoteView: View {
                             .padding(12)
                             .background(.ultraThinMaterial)
                     }
-                    .frame(width: geo.size.width / 1.4, height: geo.size.height / 2)
+                    .frame(width: geo.size.width / 1.4, height: geo.size.height / 2.2)
                     .clipShape(.rect(cornerRadius: 20))
                     
                     Button {
@@ -60,15 +62,18 @@ struct QuoteView: View {
                             .padding(.horizontal, 24)
                             .padding(.vertical, 20)
                             .frame(width: geo.size.width / 1.1)
-                            .background(.brown.opacity(0.9))
+                            .background(.breakingBadGreen.opacity(0.9))
                             .foregroundStyle(.white)
-                            .font(.system(size: 20, weight: .semibold))
+                            .font(.system(size: 22, weight: .semibold))
                             .clipShape(.capsule)
                             .shadow(color: Color.black.opacity(0.1), radius: 20, x: 0, y: 10)
                             .shadow(color: Color.black.opacity(0.3), radius: 16, x: 0, y: 2)
                     }
-                    .padding(.top, 16)
+                    .padding(.top, 8)
+                    
+                    Spacer(minLength: 100)
                 }
+                .frame(width: geo.size.width, height: geo.size.height)
             }
             .frame(width: geo.size.width, height: geo.size.height)
         }
